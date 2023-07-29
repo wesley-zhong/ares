061a3e491496bc3aa10b84329ac1d46cfd4eac24
wesley <wiqi.zhong@gmail.com>
2023-07-29 17:24:11 +0800

    proto to common

--- gateway/src/main/java/com/ares/gateway/client/TcpClient.java
+++ gateway/src/main/java/com/ares/gateway/client/TcpClient.java
@@ -5,18 +5,27 @@ import com.ares.transport.client.AresTcpClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.nio.channels.Channel;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 @Slf4j
 public class TcpClient  implements InitializingBean {
+    private final Map<Integer, Channel> channelMap = new HashMap<>();
 
     @Autowired
     private GameServerInfoList gameServerInfoList;
 
     @Autowired
     private AresTcpClient  aresTcpClient;
+
+
+
+
+
     @Override
     public void afterPropertiesSet() throws Exception {
         log.info("------ tcp client init success");
