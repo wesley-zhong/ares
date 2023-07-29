a89c81ab96e5767ca478286c23c073b4a063e5f2
wesley <wiqi.zhong@gmail.com>
2023-07-29 23:00:58 +0800

    proto to common

--- world/src/main/java/com/ares/world/WorldApplication.java
+++ world/src/main/java/com/ares/world/WorldApplication.java
@@ -6,11 +6,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+
 @SpringBootApplication
 @EnableAresTcpServer
 @ComponentScan("com.ares")
-public class WorldApplication
-{
+public class WorldApplication {
     public static void main(String[] args) {
         SpringApplication.run(WorldApplication.class, args);
     }
